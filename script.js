d6abd17efecfb382a32aa4364ff73162d1f20afd
garvitdhuran <garvitdhuran@gmail.com>
2026-09-22 15:40:00 +0530

    why aint the pomo workingggg

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const time =document.getElementById("time")
-const timeformat  =  document.getElementById("timeformat")
+const cornerTime =document.getElementById("corner-time")
+// const timeformat  =  document.getElementById("timeformat")
 
 document.addEventListener('DOMContentLoaded', ()=>{
     showTime();
@@ -11,6 +11,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById("stop-btn").onclick = stopFunction;
 
     document.getElementById("reset-btn").onclick = resetFunction;
+
+
+
+    document.getElementById("pomo-start-btn").onclick = pomoStartFunction;
+
+    document.getElementById("pomo-pause-btn").onclick = pomoPauseFunction;
+
+
+    document.getElementById("pomo-reset-btn").onclick = pomoResetFuntion;
+
 
         
 
@@ -24,8 +34,7 @@ const showTime = () => {
     let min = date.getMinutes();
 
 
-    time.innerHTML = `${hr} : ${min} : ${sec}`;
-    console.log("hours" + hr + "mins" + min + "sec" + sec );
+    cornerTime.innerHTML = `${String(hr).padStart(2,"0")}:${String(min).padStart(2,"0")}:${String(sec).padStart(2,"0")}`;
 }
 
 let seconds = 0 ;
@@ -77,3 +86,62 @@ const resetFunction = () => {
 
 };
 
+let pomosec = 2700; //thats 45 mins ig 
+
+let pomotimer = null ;
+
+
+isbreak = false;
+
+
+const formatTime2 = (totalSeconds) => {
+    let mins = Math.floor(totalSeconds/60);
+
+    let secs = totalSeconds % 60 ;
+    return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+};
+
+const pomoStartFunction = () => {
+    if(pomotimer === null){
+        pomotimer = setInterval(() => {
+            pomosec--;
+
+
+            if(pomosec <=0 ){
+                isbreak = !isbreak ;
+
+                if(isbreak){
+                    pomosec = 300;
+                    pomoLabel.innerHTML = "break time beachesss ";
+
+                } else {
+                    pomosec = 2700;
+                    pomoLabel.innerHTML = "Time to get back to work!!!!";
+                }
+            }
+
+
+
+
+            pomoDisplay.innerHTML = formatTime2(pomosec);
+        },1000);
+    }
+};
+
+
+
+const pomoStopFucntion = () => { 
+    clearIntrval(pomotimer);
+    pomotimer = null 
+};
+
+
+
+const pomoReseFunction = () => {
+    clearInterval(pomotimer);
+    pomotimer = null ;
+    isbreak = false; 
+    pomosec = 2700;
+    pomoLabel.innerHTML = "Time to get back to work!!!!";
+    pomoDisplay.innerHTML="45:00"
+};
